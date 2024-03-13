70dba03f6619d78fa06a745865431b25f118b39f
Freddy Tomada <freddytomada7@gmail.com>
2024-03-13 11:31:03 -0400

    add info component in board page

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,19 +1,12 @@
-import { create } from "@/actions/create-board";
-import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
-import Form from "./_components/form";
+import { Info } from "./_components/info";
 
 export default async function OrganizationIdPage() {
   const boards = await db.board.findMany();
 
   return (
-    <div className="flex flex-col space-y-4">
-        <Form />
-        <div className="space-y-2">
-          {boards.map((board) => (
-            <div key={board.id}>{board.title}</div>
-          ))}
-        </div>
+    <div className="flex flex-col space-y-4 w-full mb-20">
+      <Info />
     </div>
   );
 }
